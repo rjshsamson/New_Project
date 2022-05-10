d82a5e7b09f804e8c52bc74b23e458004d9b4ce4
rjshsamson <rjshsamson@gmail.com>
2022-05-10 15:23:53 +0530

    Java Training Program

---
+++ src/mycar/cardetails.java
@@ -0,0 +1,25 @@
+package mycar;
+
+public class cardetails {
+	
+	private void addition()
+	
+	{
+	
+	
+	int a=20;
+	int b=30;
+	int c=a+b;
+	
+	System.out.println(c);
+	
+}
+
+public static void main(String[] args)
+{
+	cardetails d=new cardetails();
+	d.addition();
+
+	
+}
+}
---
+++ src/sample/Sample_Pgm.java
@@ -0,0 +1,18 @@
+package sample;
+
+public class Sample_Pgm {
+	
+	private void listofflowers() {
+		
+	System.out.println("rose");
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	Sample_Pgm f= new Sample_Pgm();
+	f.listofflowers();
+		
+	}
+
+}
